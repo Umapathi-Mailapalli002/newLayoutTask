25cf25a26887d1252316972879eff4e9c5957e19
Umapathi-Mailapalli002 <ravimailapalli62@gmail.com>
2024-09-02 22:27:32 +0530

    newTask

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+
+function App() {
+
+  return (
+    <>
+      <div className='bg-gradient-to-b from-[#262b30] to-black h-[100vh]'>
+
+      </div>
+    </>
+  )
+}
+
+export default App
